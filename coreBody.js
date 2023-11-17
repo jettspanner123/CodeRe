20ee72768a4469da1f3a84d0a03d11fce97e4994
jettspanner123 <uddeshyasingh12bsci@gmail.com>
2023-11-17 00:28:47 -0800

    second final

--- coreBody.js
+++ coreBody.js
@@ -1,6 +1,26 @@
 const coreBody = document.getElementById("coreBody");
-
-
+const coreBody2 = document.getElementById("coreBody2");
+const rightButton = document.getElementById("rightButton");
+const imageDiv = document.getElementById("photoSlider")
 coreBody.addEventListener("mousemove", e => {
-    coreBody.style.background = `radial-gradient(circle farthest-side at ${e.clientX}px ${e.clientY}px, #8391ff 0%, transparent 50%)`
+    coreBody.style.background = `radial-gradient(circle farthest-side at ${e.clientX}px ${e.clientY}px, #00176c 0%, transparent 50%)`
+
+})
+
+
+let currentIndex = 1;
+rightButton.addEventListener("click" , e => {
+   imageDiv.style.transform = `translate(-${currentIndex * 30}rem)`;
+   rightButton.style.scale = "0.5";
+   currentIndex++;
+   rightButton.style.scale = "1";
+   if(currentIndex >= 4){
+      currentIndex = 0;
+   }
+})
+
+
+coreBody2.addEventListener("mousemove", e => {
+   coreBody.style.background = `radial-gradient(circle farthest-side at ${e.clientX}px ${e.clientY}px, #00176c 0%, transparent 50%)`
+
 })
